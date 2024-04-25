a7edd3bfb4a11c6908c1d273d58a3c6978cd70d8
Wanderley Cardoso Celeste <cawander@gmail.com>
2024-04-25 10:49:10 -0300

    Initial

---
+++ Receitas/urls.py
@@ -0,0 +1,39 @@
+"""
+URL configuration for Receitas project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponse
+
+
+def my_view_home(request):
+    return HttpResponse('Visualização Home!')
+
+
+def my_view_sobre(request):
+    return HttpResponse('Visualização Sobre')
+
+
+def my_view_contato(request):
+    return HttpResponse('Visualização Contato')
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', my_view_home),
+    path('sobre/', my_view_sobre),
+    path('contato/', my_view_contato),
+]

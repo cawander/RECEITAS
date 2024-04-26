d56a33799fd3108ef2a66e56ec3a1f523ff12fff
Wanderley Cardoso Celeste <cawander@gmail.com>
2024-04-26 11:23:02 -0300

    Header formatted and separated of page's body.

--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 def view_home(request):
     # return render(request, 'global/home.html')
-    return render(request, template_name='recipes/home.html',
+    return render(request, template_name='recipes/pages/home.html',
                   context={
                       'mensagem': 'Wanderley testando o contexto',
                   }, status=201

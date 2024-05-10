4fa6ff246934ac6379ebad143b484749f2982127
Wanderley Cardoso Celeste <cawander@gmail.com>
2024-05-10 10:04:15 -0300

    To make category URLs unique and return a 404 error if a non-existing category is requested.

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_list_or_404
 from utils.recipes.factory import make_recipe
 from .models import Recipe
 
@@ -8,25 +7,30 @@ def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, template_name='recipes/pages/home.html',
                   context={'recipes': recipes})
-    # return render(request, template_name='recipes/pages/home.html',
-    #              context={
-    #                 'recipes': [make_recipe() for _ in range(10)],
-    #             }, status=201
-    #             )
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id, is_published=True).order_by('-id')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True,
+        ).order_by('-id')
+    )
+
     return render(request, template_name='recipes/pages/category.html',
-                  context={'recipes': recipes})
+                  context={
+                      'recipes': recipes,
+                      'title': f'{recipes[0].category.name} - Category | ',
+                  }
+                  )
 
 
 def recipe(request, id):
-    # return render(request, 'global/home.html')
-    return render(request, template_name='recipes/pages/recipe-view.html',
-                  context={
-                      'recipe': make_recipe(),
-                      'is_detail_page': True,
-                  }, status=201
-                  )
+
+    return render(
+        request,
+        template_name='recipes/pages/recipe-view.html',
+        context={
+            'recipe': make_recipe(), 'is_detail_page': True,
+        }, status=201
+    )

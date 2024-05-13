bb09f845acb84da9a4557be6219fa6cba252405f
Wanderley Cardoso Celeste <cawander@gmail.com>
2024-05-13 17:13:57 -0300

    Starting pytest-django

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class RecipeURLsTest(TestCase):
+    def test_the_pytest_is_ok(self):
+        variavel = '123456'
+        # assert 1 == 2, '1 não é igual a 2!'
+        assert 1 == 1, '1 não é igual a 1!'

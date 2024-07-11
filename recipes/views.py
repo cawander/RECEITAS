fdee9e7806d3aa04d6c80fabf02276105f5c8bcd
Wanderley Cardoso Celeste <cawander@gmail.com>
2024-07-11 12:10:54 -0300

    Flash message mechanism implemented.

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.http import Http404
 from django.db.models import Q
+from django.contrib import messages
 # from utils.recipes.factory import make_recipe
 from .models import Recipe
 from utils.recipes.pagination import make_pagination
@@ -18,6 +19,10 @@ def home(request):
 
     page_obj, pagination_range = make_pagination(
         request, recipes, PER_PAGE, QTY_PAGES)
+
+    messages.success(request, 'SUCESSO!')
+    messages.error(request, 'DEU MERDA!')
+    messages.info(request, 'FIQUE LIGADO!')
 
     return render(
         request,
